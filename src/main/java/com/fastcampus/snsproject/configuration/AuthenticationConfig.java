9ea1eb2829580a22f3b254c0fe05d2abfe1df83e
krekar83 <krekar83@gmail.com>
2023-02-02 13:55:14 +0900

    [개발] part8. ch03. "03.좋아요 기능 테스트 작성하기"까지 진행

--- src/main/java/com/fastcampus/snsproject/configuration/AuthenticationConfig.java
+++ src/main/java/com/fastcampus/snsproject/configuration/AuthenticationConfig.java
@@ -6,7 +6,9 @@ import com.fastcampus.snsproject.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,6 +22,11 @@ public class AuthenticationConfig extends WebSecurityConfigurerAdapter {
     private final UserService userService;
     @Value("${jwt.secret-key}")
     private String key;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().regexMatchers("^(?!/api/).*");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
