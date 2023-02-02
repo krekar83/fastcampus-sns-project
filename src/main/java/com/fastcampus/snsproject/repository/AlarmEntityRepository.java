279e09371fde9701768cfd6f622553bdc29742c5
krekar83 <krekar83@gmail.com>
2023-02-02 21:58:16 +0900

    [개발] part8. ch04. "02.코드 최적화하기"까지 진행

--- src/main/java/com/fastcampus/snsproject/repository/AlarmEntityRepository.java
+++ src/main/java/com/fastcampus/snsproject/repository/AlarmEntityRepository.java
@@ -1,7 +1,6 @@
 package com.fastcampus.snsproject.repository;
 
 import com.fastcampus.snsproject.model.entity.AlarmEntity;
-import com.fastcampus.snsproject.model.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AlarmEntityRepository extends JpaRepository<AlarmEntity, Integer> {
 
-    Page<AlarmEntity> findAllByUser(UserEntity user, Pageable pageable);
+    Page<AlarmEntity> findAllByUserId(Integer userId, Pageable pageable);
 }
--- src/main/java/com/fastcampus/snsproject/service/PostService.java
+++ src/main/java/com/fastcampus/snsproject/service/PostService.java
@@ -58,7 +58,8 @@ public class PostService {
         if (postEntity.getUser() != userEntity) {
             throw new SnsApplicationException(ErrorCode.INVALID_PERMISSION, String.format("%s has no permission with %s", userName, postId));
         }
-
+        likeEntityRepository.deleteAllByPost(postEntity);
+        commentEntityRepository.deleteAllByPost(postEntity);
         postEntityRepository.delete(postEntity);
     }
 
@@ -87,7 +88,7 @@ public class PostService {
         alarmEntityRepository.save(AlarmEntity.of(postEntity.getUser(), AlarmType.NEW_LIKE_ON_POST, new AlarmArgs(userEntity.getId(), postEntity.getId())));
     }
 
-    public int likeCount(Integer postId) {
+    public long likeCount(Integer postId) {
         PostEntity postEntity = getPostOrException(postId);
 
         // count liked
